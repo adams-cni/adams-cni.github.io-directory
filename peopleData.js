3c9b4820e3363380c31e576f500dda11a23286f7
Andrew Adams <aadams@capitolnewsillinois.com>
2023-07-11 01:47:35 -0500

    Create peopleData.js

---
+++ peopleData.js
@@ -0,0 +1,101 @@
+var peopleData = [{
+        id: '1',
+        name: 'Andrew Adams',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+        address: '124 N Road Ave',
+        city: 'Chicago',
+        category: 'Legislative',
+      },
+      {
+        id: '2',
+        name: 'Beth Huntsdorfer',
+        email: 'jane@example.com',
+        phone: '987-654-3210',
+          address: '124 N Road Ave',
+        city: 'Chicago',
+        category: 'State Agencies',
+      },
+      {
+          id: '3',
+        name: 'Hannah Meisel',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Chicago',
+        category: 'Constitutional Officers',
+      },
+      {
+          id: '4',
+        name: 'Peter Hancock',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Chicago',
+        category: 'Judicial'
+      },
+      {
+          id: '5',
+        name: 'Grace Kinnicut',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Galena',
+        category: 'Constitutional Officers'
+      },
+      {
+          id: '6',
+        name: 'Grace Barbic',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Aurora',
+        category: 'Constitutional Officers'
+      },
+      {
+          id: '7',
+        name: 'Nika Schoonover',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Geneva',
+        category: 'Judicial'
+      },
+      {
+          id: '8',
+        name: 'Jen Fuller',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Downers Grove',
+        category: 'Legislative'
+      },
+      {
+          id: '9',
+        name: 'Jane Lynch',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Springfield',
+        category: 'Legislative'
+      },
+      {
+          id: '10',
+        name: 'Jeff Rogers',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Champaign',
+        category: 'Legislative'
+      },
+      {
+          id: '11',
+        name: 'Raymon Troncoso',
+        email: 'john@example.com',
+        phone: '123-456-7890',
+          address: '124 N Road Ave',
+        city: 'Topkea',
+        category: 'Legislative'
+      },
+      // ... Add the remaining 295 people data objects here
+    ];
